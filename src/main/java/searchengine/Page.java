bf6e7eb39f3b213e82d235d15f564b1077947bcd
beppek <beppe@live.se>
2017-12-05 18:33:31 +0100

    page class

--- src/main/java/searchengine/Page.java
+++ src/main/java/searchengine/Page.java
@@ -1,4 +1,20 @@
 package searchengine;
 
+import java.util.ArrayList;
+
 public class Page {
+    private String url;
+    private ArrayList<Integer> words;
+    Page(String url, ArrayList<Integer> words) {
+        this.url = url;
+        this.words = words;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public ArrayList<Integer> getWords() {
+        return words;
+    }
 }

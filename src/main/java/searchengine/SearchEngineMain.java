668cdc50d8f5d9504d201ad04e2ce63daa9dce3c
beppek <beppe@live.se>
2017-12-05 18:35:20 +0100

    add main class to run engine without spring

--- src/main/java/searchengine/SearchEngineMain.java
+++ src/main/java/searchengine/SearchEngineMain.java
@@ -1,4 +1,8 @@
 package searchengine;
 
 public class SearchEngineMain {
+    public static void main(String[] args) {
+        SearchEngine engine = new SearchEngine();
+        engine.query("blockchain bitcoin sweden".toLowerCase());
+    }
 }

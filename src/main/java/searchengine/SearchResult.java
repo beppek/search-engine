607ae6fe2e2a787c7130b281d776e469d5ee228e
beppek <beppe@live.se>
2017-12-05 18:35:05 +0100

    create search result class

--- src/main/java/searchengine/SearchResult.java
+++ src/main/java/searchengine/SearchResult.java
@@ -1,4 +1,30 @@
 package searchengine;
 
-public class SearchResult {
+public class SearchResult implements Comparable<SearchResult> {
+
+    private double score;
+    private Page page;
+
+    SearchResult(Page page, double score) {
+        this.page = page;
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public Page getPage() {
+        return page;
+    }
+
+    @Override
+    public int compareTo(SearchResult compareTo) {
+        return Double.compare(compareTo.getScore(), score);
+    }
+
+    @Override
+    public String toString() {
+        return "score: " + score + " url: " + page.getUrl();
+    }
 }

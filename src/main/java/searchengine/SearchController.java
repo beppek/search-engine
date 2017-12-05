890d6213564af5b0542d14d88844d42ebabac532
beppek <beppe@live.se>
2017-12-05 18:34:07 +0100

    start working on search controller

--- src/main/java/searchengine/SearchController.java
+++ src/main/java/searchengine/SearchController.java
@@ -1,4 +1,20 @@
 package searchengine;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+@RestController
+@CrossOrigin()
 public class SearchController {
+
+    @RequestMapping("/")
+    public ArrayList<SearchResult> Search(@RequestParam(value="query") String query) {
+        SearchEngine engine = new SearchEngine();
+        ArrayList<SearchResult> results = engine.query(query);
+        return results;
+    }
 }

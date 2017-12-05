fe2c499ffc2f5c5620330bddbf07c1339a3519e6
beppek <beppe@live.se>
2017-12-05 18:33:38 +0100

    page db class

--- src/main/java/searchengine/PageDB.java
+++ src/main/java/searchengine/PageDB.java
@@ -1,4 +1,29 @@
 package searchengine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PageDB {
+    private Map<String, Integer> wordToId = new HashMap<String, Integer>();
+    private List<Page> pages = new ArrayList<Page>();
+
+    public int getIdForWord(String word) {
+        if (wordToId.containsKey(word)) {
+            return wordToId.get(word);
+        } else {
+            int id = wordToId.size();
+            wordToId.put(word, id);
+            return id;
+        }
+    }
+
+    public List<Page> getPages() {
+        return pages;
+    }
+
+    public void addPage(Page page) {
+        pages.add(page);
+    }
 }

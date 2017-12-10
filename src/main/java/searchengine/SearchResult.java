2c5a124c6b40edbd5f7b0d91724d639fd3965e82
beppek <beppe@live.se>
2017-12-10 16:18:03 +0100

    update search result object

--- src/main/java/searchengine/SearchResult.java
+++ src/main/java/searchengine/SearchResult.java
@@ -1,21 +1,54 @@
 package searchengine;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class SearchResult implements Comparable<SearchResult> {
 
-    private double score;
+    @JsonIgnore
     private Page page;
+    private String url;
+    private double score;
+    private double pageRank;
+    private double frequency;
+    private double location;
+    private double distance;
 
-    SearchResult(Page page, double score) {
+    SearchResult(Page page, double score, double frequency, double location, double distance) {
         this.page = page;
+        this.url = page.getFullUrl();
+        this.pageRank = page.getPageRank();
+        this.frequency = frequency;
+        this.location = location;
+        this.distance = distance;
         this.score = score;
+    }
+
+    public String getUrl() {
+        return url;
     }
 
     public double getScore() {
         return score;
     }
 
+    public double getPageRank() {
+        return pageRank;
+    }
+
     public Page getPage() {
         return page;
+    }
+
+    public double getLocation() {
+        return location;
+    }
+
+    public double getFrequency() {
+        return frequency;
+    }
+
+    public double getDistance() {
+        return distance;
     }
 
     @Override
@@ -25,6 +58,6 @@ public class SearchResult implements Comparable<SearchResult> {
 
     @Override
     public String toString() {
-        return "score: " + score + " pagerank: " + page.getPageRank() + " url: " + page.getFullUrl();
+        return "score: " + score + " Page Rank: " + pageRank + " url: " + page.getFullUrl();
     }
 }

8174f066897505ba57ee90a2d01074944f2c75fc
beppek <beppe@live.se>
2017-12-11 00:20:08 +0100

    save and read index from file

--- src/main/java/searchengine/utils/FileHandler.java
+++ src/main/java/searchengine/utils/FileHandler.java
@@ -1,24 +1,41 @@
 package searchengine.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import searchengine.models.Page;
 import searchengine.models.PageDB;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.io.*;
+import java.util.*;
 
 public class FileHandler {
 
     private PageDB db;
+    private String pageIndexPath = "data/page-index.json";
 
     public FileHandler(PageDB db) {
         this.db = db;
     }
 
+    public boolean indexExists() {
+        File pf = new File(pageIndexPath);
+        return pf.exists() && !pf.isDirectory();
+    }
+
+    public void savePageDBIndex() throws IOException {
+        System.out.println("Saving index to file...");
+        savePageIndex();
+        System.out.println("Done saving index to file");
+    }
+
+    public void readDB() throws IOException {
+        System.out.println("Reading index from file...");
+        readPageIndex();
+        System.out.println("Done reading index from file");
+    }
+
     public void readDataFiles() {
+        System.out.println("Start reading pages from files...");
         ArrayList<File> files = new ArrayList<File>();
         File wordsDir = new File("data/words/");
         File[] subs = wordsDir.listFiles();
@@ -29,6 +46,78 @@ public class FileHandler {
             File lf = new File(f.getAbsolutePath().replaceFirst("words", "links"));
             generatePage(regenerateUrl(f.getName()), f, lf);
         }
+        System.out.println("Done reading pages from files");
+    }
+
+    private void savePageIndex() throws IOException {
+        File f = new File(pageIndexPath);
+        f.createNewFile();
+        BufferedWriter w = new BufferedWriter(new FileWriter(f.getAbsoluteFile()));
+        Gson gson = new Gson();
+        w.write(gson.toJson(db));
+        w.close();
+    }
+
+    private void readPageIndex() throws IOException {
+        File f = new File(pageIndexPath);
+        JsonReader reader = new JsonReader(new FileReader(f));
+        Map<String, Integer> wordToInt = new HashMap<String, Integer>();
+        reader.beginObject();
+        String n = reader.nextName();
+        System.out.println(n);
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String word = reader.nextName();
+            Integer id = reader.nextInt();
+            db.addWordFromFile(word, id);
+            wordToInt.put(word, id);
+        }
+        reader.endObject();
+        String p = reader.nextName();
+        System.out.println(p);
+
+        reader.beginArray();
+        int i = 0;
+        ArrayList<Page> pages = new ArrayList<Page>();
+        while (reader.hasNext()) {
+            reader.beginObject();
+            while (reader.hasNext()) {
+                String name = reader.nextName();
+                if (name.equals("url")) {
+                    String url = reader.nextString();
+                    pages.add(new Page(url));
+                } else if (name.equals("words")) {
+                    reader.beginArray();
+                    ArrayList<Integer> words = new ArrayList<Integer>();
+                    while (reader.hasNext()) {
+                        words.add(reader.nextInt());
+                    }
+                    pages.get(i).setWords(words);
+                    reader.endArray();
+                } else if (name.equals("links")) {
+                    reader.beginArray();
+                    Set<String> links = new HashSet<String>();
+                    while (reader.hasNext()) {
+                        links.add(reader.nextString());
+                    }
+                    pages.get(i).setLinks(links);
+                    reader.endArray();
+                } else if (name.equals("pageRank")) {
+                    Double pageRank = reader.nextDouble();
+                    pages.get(i).setPageRank(pageRank);
+                } else if (name.equals("linksCount")) {
+                    int linksCount = reader.nextInt();
+                    pages.get(i).setLinksCount(linksCount);
+                    i++;
+                } else {
+                    String s = reader.nextString();
+                }
+            }
+            reader.endObject();
+        }
+        reader.endArray();
+        System.out.println(pages.size());
+        db.addAllPages(pages);
     }
 
     private String regenerateUrl(String filename) {
